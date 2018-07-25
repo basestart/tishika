06a583fa1e00b9cbfb9b7140c7d30d38c7620704
willamena <willamena@126.com>
2018-07-26 01:24:18 +0800

    feat: move from jq to vue

--- source/js/map.js
+++ source/js/map.js
@@ -1,5 +1,4 @@
 
-
 var myLatlng = new qq.maps.LatLng(39.916527,116.397128);
 var myOptions = {
     zoom: 15,
@@ -7,5 +6,10 @@ var myOptions = {
     mapTypeId: qq.maps.MapTypeId.ROADMAP
 }
 var map = new qq.maps.Map(document.getElementById("map"), myOptions);
+
+qq.maps.event.addListener(map, 'click', function(e) {
+    console.log(e);
+    alert('您点击了地图.');
+});
 
 // get location
